ea17aba787df62052c17072d631f90b67db114fe
krzyswo <80654546+krzyswo@users.noreply.github.com>
2025-01-30 15:21:38 +0100

    Create EXO REPORTS.ps1

---
+++ EXO REPORTS/EXO REPORTS.ps1
@@ -0,0 +1,35 @@
+# Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName admin@example.com
+
+# Get a list of all mailboxes
+Get-Mailbox
+
+# Get a list of mailboxes that have been inactive for more than 90 days
+Get-StaleMailboxReport -InactiveMailboxDays 90
+
+# Get a list of mailboxes that have not been logged into in the past 30 days
+Get-InactiveMailboxReport -InactiveDays 30
+
+# Get a list of mailboxes with large items (over 10 MB)
+Get-LargeItemMailboxReport -SizeLimit 10MB
+
+# Get a list of mailboxes with a forwarding address configured
+Get-Mailbox | Where-Object {$_.ForwardingSmtpAddress -ne $null}
+
+# Get a list of mailboxes that have delegates
+Get-Mailbox | Where-Object {$_.DelegateType -ne "None"}
+
+# Get a list of mailboxes with full access permissions
+Get-Mailbox | Where-Object {$_.FullAccess -ne $null}
+
+# Get a list of mailboxes with send-as permissions
+Get-Mailbox | Where-Object {$_.GrantSendOnBehalfTo -ne $null}
+
+# Get a list of mailboxes with Send on Behalf permissions
+Get-Mailbox | Where-Object {$_.GrantSendOnBehalfTo -ne $null}
+
+# Get a list of mailboxes with specific email addresses
+Get-Mailbox | Where-Object {$_.EmailAddresses -like "*@example.com"}
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline

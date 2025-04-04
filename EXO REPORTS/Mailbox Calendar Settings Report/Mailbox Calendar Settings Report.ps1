be6a66b4f8b149027520026c2679e4842b4cd2a8
Krzyswo <80654546+krzyswo@users.noreply.github.com>
2025-04-04 11:04:19 +0200

    Update Mailbox Calendar Settings Report.ps1

--- EXO REPORTS/Mailbox Calendar Settings Report/Mailbox Calendar Settings Report.ps1
+++ EXO REPORTS/Mailbox Calendar Settings Report/Mailbox Calendar Settings Report.ps1
@@ -1,1 +1,63 @@
-
+$settings = @()
+ 
+foreach ($obj in $list)
+{
+    $mailbox = get-mailbox -Identity $obj |select DisplayName,UserPrincipalName,PrimarySmtpAddress
+    $processing = get-calendarprocessing -Identity $obj
+ 
+    $settings += [PSCustomObject]@{
+       
+        DisplayName                           = $mailbox.DisplayName
+        UserPrincipalName                     = $mailbox.UserPrincipalName
+        PrimarySmtpAddress                    = $mailbox.PrimarySmtpAddress
+        AutomateProcessing                    = $processing.AutomateProcessing
+        AllowConflicts                        = $processing.AllowConflicts
+        AllowDistributionGroup                = $processing.AllowDistributionGroup
+        AllowMultipleResources                = $processing.AllowMultipleResources
+        BookingType                           = $processing.BookingType
+        BookingWindowInDays                   = $processing.BookingWindowInDays
+        MaximumDurationInMinutes              = $processing.MaximumDurationInMinutes
+        MinimumDurationInMinutes              = $processing.MinimumDurationInMinutes
+        AllowRecurringMeetings                = $processing.AllowRecurringMeetings
+        EnforceAdjacencyAsOverlap             = $processing.EnforceAdjacencyAsOverlap
+        EnforceCapacity                       = $processing.EnforceCapacity
+        EnforceSchedulingHorizon              = $processing.EnforceSchedulingHorizon
+        ScheduleOnlyDuringWorkHours           = $processing.ScheduleOnlyDuringWorkHours
+        ConflictPercentageAllowed             = $processing.ConflictPercentageAllowed
+        MaximumConflictInstances              = $processing.MaximumConflictInstances
+        ForwardRequestsToDelegates            = $processing.ForwardRequestsToDelegates
+        DeleteAttachments                     = $processing.DeleteAttachments
+        DeleteComments                        = $processing.DeleteComments
+        RemovePrivateProperty                 = $processing.RemovePrivateProperty
+        DeleteSubject                         = $processing.DeleteSubject
+        AddOrganizerToSubject                 = $processing.AddOrganizerToSubject
+        DeleteNonCalendarItems                = $processing.DeleteNonCalendarItems
+        TentativePendingApproval              = $processing.TentativePendingApproval
+        EnableResponseDetails                 = $processing.EnableResponseDetails
+        OrganizerInfo                         = $processing.OrganizerInfo
+        ResourceDelegates                     = $processing.ResourceDelegates
+        RequestOutOfPolicy                    = $processing.RequestOutOfPolicy
+        AllRequestOutOfPolicy                 = $processing.AllRequestOutOfPolicy
+        BookInPolicy                          = $processing.BookInPolicy
+        AllBookInPolicy                       = $processing.AllBookInPolicy
+        RequestInPolicy                       = $processing.RequestInPolicy
+        AllRequestInPolicy                    = $processing.AllRequestInPolicy
+        AddAdditionalResponse                 = $processing.AddAdditionalResponse
+        AdditionalResponse                    = $processing.AdditionalResponse
+        RemoveOldMeetingMessages              = $processing.RemoveOldMeetingMessages
+        AddNewRequestsTentatively             = $processing.AddNewRequestsTentatively
+        ProcessExternalMeetingMessages        = $processing.ProcessExternalMeetingMessages
+        RemoveForwardedMeetingNotifications   = $processing.RemoveForwardedMeetingNotifications
+        AutoRSVPConfiguration                 = $processing.AutoRSVPConfiguration
+        RemoveCanceledMeetings                = $processing.RemoveCanceledMeetings
+        EnableAutoRelease                     = $processing.EnableAutoRelease
+        PostReservationMaxClaimTimeInMinutes  = $processing.PostReservationMaxClaimTimeInMinutes
+        MailboxOwnerId                        = $processing.MailboxOwnerId
+        Identity                              = $processing.Identity
+        IsValid                               = $processing.IsValid
+        ObjectState                           = $processing.ObjectState
+       
+       }
+}
+ 
+$settings |out-gridview

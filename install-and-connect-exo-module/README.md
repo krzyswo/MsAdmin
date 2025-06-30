cd59c3e84734e8467c8895a20f6e8a7a1ebea543
krzyswo <your_verified_email@example.com>
2025-06-30 06:40:54 +0000

    Released: script from staging

---
+++ install-and-connect-exo-module/README.md
@@ -0,0 +1,20 @@
+## Name: install-and-connect-exo-module
+
+## Description
+This script automatically installs the Exchange Online Management module and connects to Exchange Online PowerShell.
+
+## Prerequisites
+- PowerShell must be installed on your system.
+- You must have administrative privileges on your system.
+
+## Usage
+Run the script to install the Exchange Online PowerShell V3 module and connect to Exchange Online PowerShell using modern authentication methods.
+
+## Output
+The script facilitates administration tasks by securely connecting to Exchange Online PowerShell.
+
+## Notes
+This script is intended for use by system administrators managing Microsoft 365 environments.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ install-and-connect-exo-module/install-and-connect-exo-module.ps1
@@ -0,0 +1,30 @@
+﻿<#
+=============================================================================================
+Name:           Install and Connect to Exchange Online PowerShell Module
+Version:        3.0
+Website:        https://github.com/krzyswo/MsAdmin
+For detailed Script execution: https://github.com/krzyswo/MsAdmin
+============================================================================================
+#>
+
+#Due to RPS and Basic Auth retirement in Exchange Online, we need the EXO V3 module
+$Module = (Get-Module ExchangeOnlineManagement -ListAvailable) | where {$_.Version.major -ge 3}
+if($Module.count -eq 0)
+{
+ Write-Host 'Exchange Online PowerShell V3 module is not available' -ForegroundColor yellow
+ $Confirm= Read-Host 'Are you sure you want to install module? [Y] Yes [N] No'
+ if($Confirm -match "[yY]")
+ {
+ Write-host "Installing Exchange Online PowerShell module"
+ Install-Module ExchangeOnlineManagement -Repository PSGallery -AllowClobber -Force
+ Import-Module ExchangeOnlineManagement
+ }
+ else
+ {
+ Write-Host 'EXO V3 module is required to connect to Exchange Online. Please install module using Install-Module ExchangeOnlineManagement cmdlet.'
+ Exit
+ }
+}
+ 
+Write-Host 'Connecting to Exchange Online...'
+Connect-ExchangeOnline
